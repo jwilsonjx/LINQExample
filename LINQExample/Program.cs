f89a4006d70ae8865bb743400c9ff5306ac11f17
Jason Wilson <jwilsonjx@outlook.com>
2017-05-30 14:01:31 -0400

    Add project files.

---
+++ LINQExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LINQExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("*****************");
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5); 
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20}: {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files =  directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+
+                Console.WriteLine($"{file.Name, -20}: {file.Length, 10:N0}");
+            }  
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
